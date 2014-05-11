db76a12894373d65a7436420d25021ddf93bd480
matbhz <matbhz@gmail.com>
2014-05-11 07:55:52 -0700

    Adds endpoint /create that receives a JSON with the attributes 'name' and 'pass' in the Request Body

--- src/GoAuth.go
+++ src/GoAuth.go
@@ -1,11 +1,32 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
+)
 
 func main() {
 	m := martini.Classic()
-	m.Get("/", func() string {
-			return "Hello world!"
-		})
+	m.Get("/hello/:name", helloWorld)
+	m.Post("/create", binding.Json(User{}), createUser)
+
 	m.Run()
+}
+
+func helloWorld(params martini.Params) string {
+	return "Hello, " + params["name"] + "!"
+}
+
+func createUser(user User) (int, string) {
+	if len(user.Name) <= 0 || len(user.Pass) <= 0 {
+		return 500, "User not created!"
+	}
+
+	// TODO: Save to the DB
+	return 201, "User created successfully!"
+}
+
+type User struct {
+	Name string `json:"name" binding:"required"`
+	Pass string `json:"pass" binding:"required"`
 }
