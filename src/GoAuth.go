fd19b726e362b7ff9fb5fb0feb2b199616f93e57
matbhz <matbhz@gmail.com>
2014-05-11 17:13:40 -0700

    Adds MD5 checksum to be stored in Redis and files to .gitignore

--- src/GoAuth.go
+++ src/GoAuth.go
@@ -6,15 +6,9 @@ import (
 	"github.com/martini-contrib/render"
 	"github.com/fzzy/radix/redis"
 	"log"
+	"io"
+	"crypto/md5"
 )
-
-func errorHandler(error error) {
-	if (error != nil) {
-		log.Println("AN ERROR HAS OCCURED:")
-		log.Println(error)
-	}
-}
-
 
 func main() {
 	m := martini.Classic()
@@ -23,13 +17,13 @@ func main() {
 	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
 	errorHandler(err)
 	defer conn.Close()
-
 	m.Map(conn)
 
 	// Endpoints
 	m.Get("/hello/:name", helloWorld)
 	m.Post("/create", binding.Json(User{}), createUser)
 	m.Post("/authenticate", binding.Json(User{}), authenticateUser)
+
 	// Start the server
 	m.Run()
 }
@@ -42,16 +36,16 @@ func createUser(user User, r render.Render, conn *redis.Client) {
 	if (len(user.Name) <= 0 || len(user.Pass) <= 0 || hasUser(user, conn)) {
 		r.JSON(500, "User not created.")
 	} else {
-		conn.Cmd("set", user.Name, user.Pass)
+		saveUser(user, conn)
 		r.JSON(201, "User created successfully!")
 	}
 }
 
-func authenticateUser(user User, r render.Render, conn * redis.Client) {
+func authenticateUser(user User, r render.Render, conn *redis.Client) {
 
-	fetchedUser := getUser(user, conn)
+	fetchedUser := retrieveUser(user, conn)
 
-	if ( fetchedUser.Pass == user.Pass ) {
+	if (userToMD5(user) == fetchedUser.Pass) {
 		r.JSON(200, "Welcome, "+user.Name+"!")
 	} else {
 		r.JSON(401, "User or password incorrect.")
@@ -59,14 +53,31 @@ func authenticateUser(user User, r render.Render, conn * redis.Client) {
 }
 
 func hasUser(user User, conn *redis.Client) bool {
-	return getUser(user, conn).Pass != ""
+	return retrieveUser(user, conn).Pass != ""
 }
 
-func getUser(user User, conn *redis.Client) User {
+func retrieveUser(user User, conn *redis.Client) User {
 	fetchedPassword, err := conn.Cmd("get", user.Name).Str()  // TODO: Find better way to see if the Reply is null
 	errorHandler(err)
-
 	return User{user.Name, fetchedPassword}
+}
+
+func saveUser(user User, conn *redis.Client) {
+	conn.Cmd("set", user.Name, userToMD5(user))
+}
+
+func userToMD5(user User) string {
+	md5 := md5.New()
+	io.WriteString(md5, user.Name)
+	io.WriteString(md5, user.Pass)
+	return string(md5.Sum(nil))
+}
+
+func errorHandler(error error) {
+	if (error != nil) {
+		log.Println("AN ERROR HAS OCCURED:")
+		log.Println(error)
+	}
 }
 
 type User struct {
